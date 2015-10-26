3a28cfe9f1327368a596802ee50dd3de700c64d7
怀恩2 <huaien2@huaien2deMacBook-Pro.local>
2015-10-26 18:22:27 +0800

    添加了HaiViewController

---
+++ HaixiaViewController.m
@@ -0,0 +1,37 @@
+//
+//  HaixiaViewController.m
+//  Bodhicitta
+//
+//  Created by 怀恩2 on 15/10/26.
+//  Copyright (c) 2015年 怀恩2. All rights reserved.
+//
+
+#import "HaixiaViewController.h"
+
+@interface HaixiaViewController ()
+
+@end
+
+@implementation HaixiaViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
